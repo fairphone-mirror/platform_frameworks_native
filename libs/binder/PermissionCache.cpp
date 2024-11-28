4440a23212fe998bac9767f39a68bfae125ea36a
jianglong.pan <jianglong.pan@tcl.com>
2024-11-28 18:12:44 +0800

    [FP5V-470] [Launcher][Recent screen] All app interfaces are black in recent screen
    * Root Cause
      time process issue
    * Solution
      workaround for check system process permission again
    * Test Steps
      follow description
    * Test Result
      pass
    
    Change-Id: Iae5cf077cba342e3481b995f3156068364bec0a8

--- libs/binder/PermissionCache.cpp
+++ libs/binder/PermissionCache.cpp
@@ -105,6 +105,15 @@ bool PermissionCache::checkPermission(
               granted ? "granted" : "denied", (int)ns2us(t));
         pc.cache(permission, uid, granted);
     }
+    // Add for FP5V-470
+    if (!granted && uid == 1000) {
+        nsecs_t t = -systemTime();
+        granted = android::checkPermission(permission, pid, uid);
+        t += systemTime();
+        ALOGE("checking %s for uid=%d => %s (%d us)", String8(permission).c_str(), uid,
+              granted ? "granted" : "denied", (int)ns2us(t));
+        pc.cache(permission, uid, granted);
+    }
     return granted;
 }
 
