9b6774e0b8af35f5166f2549daa24d2805a73097
jianglong.pan <jianglong.pan@tcl.com>
2025-04-10 15:17:43 +0800

    [FPS-1934]  set doze refresh to 10
    * Root Cause
      set doze refresh to 10
    * Solution
      set doze refresh to 10
    * Test Steps
      Follow description
    * Test Result
      pass
    
    Change-Id: Ie50f29b2865a817bd9737b446b1e199dcafd549e

--- services/surfaceflinger/Scheduler/RefreshRateSelector.h
+++ services/surfaceflinger/Scheduler/RefreshRateSelector.h
@@ -50,7 +50,7 @@ public:
             std::chrono::nanoseconds(800us).count();
 
     // The lowest Render Frame Rate that will ever be selected
-    static constexpr Fps kMinSupportedFrameRate = 1_Hz;
+    static constexpr Fps kMinSupportedFrameRate = 10_Hz;
     static constexpr Fps kLowestSupportedFrameRate = 20_Hz;
 
     // Start range for FrameRateCategory Normal and High.
